eb4a899d08375c5945ee765708d9ea1948e27856
Ravens <66542557+Ravens5047@users.noreply.github.com>
2024-03-28 11:48:29 +0700

    new baseapi

--- lib/components/constants/app_constant.dart
+++ lib/components/constants/app_constant.dart
@@ -2,6 +2,9 @@ class AppConstant {
   AppConstant._();
 
   static const baseAPI = 'http://192.168.1.8:3131';
+  // static const baseAPI = 'http://192.168.1.2:3131/'; //Nhà Đạt
+  // static const baseAPI = 'http://192.168.3.41:3131/'; //Cty
+
   static const endPointLogin = '$baseAPI/login';
   static const endPointRegister = '$baseAPI/users';
   static const endPointGetListServices = '$baseAPI/services';
